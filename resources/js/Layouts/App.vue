d64071644fcf8a46bf55fe2440910252fd3cb3e9
Ngozi Stephen <ngozi.stephen99@gmail.com>
2023-03-13 13:36:05 +0100

    HomePage, App Layout Page and app id to the template

---
+++ resources/js/Layouts/App.vue
@@ -0,0 +1,55 @@
+<template>
+      <header class="header header--desktop header--one header--fixed header--transparent" data-sticky="true">
+        <div class="container">
+            <div class="header__wrapper">
+                <div class="header__left">
+                    <div class="header__logo"><a class="ps-logo light" href="index.html"><img src="img/logo-white.png" alt="" /></a><a class="ps-logo" href="index.html"><img src="img/logo.png" alt="" /></a>
+                    </div>
+                    <ul class="menu menu--desktop">
+                        <li class="menu-item-has-children has-mega-menu"><a href="properties.html">Properties</a><span class="sub-toggle"></span> 
+                        </li>
+                        <li class="menu-item-has-children"><a href="blog-grid.html">Blog</a><span class="sub-toggle"></span>
+                        </li>
+                        <li><a href="agents.html">Agents</a>
+                        </li>
+                        <li><a href="profile.html">My Account</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="header__right">
+                    <div class="header__actions">
+                        <div class="header__account"><a href="signin.html"><i class="lnil lnil-user"></i>Log In</a></div>
+                        <div class="header__submit-listing">
+                            <button class="ps-btn ps-btn--with-opacity">Submit Listing<i class="lnil lnil-arrow-top-right"></i></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <header class="header header--mobile" data-sticky="true">
+        <div class="header__left">
+            <div class="header__logo"><a class="ps-logo" href="index.html"><img src="img/logo.png" alt="" /></a>
+            </div>
+        </div>
+        <div class="header__right"><a class="header__menu-toggle ps-toggle-drawer" href="#" data-target="drawer-menu"><i class="lnil lnil-menu"></i></a></div>
+    </header>
+    <home-page></home-page>
+    <!-- Plugins-->
+</template>
+
+<script>
+    import HomePage from '../components/HomePage.vue';
+
+    
+    export default {
+       
+
+
+        components: {
+           HomePage
+        },
+
+        
+    }
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -2,12 +2,14 @@ import './bootstrap';
 
 import {createApp} from 'vue'
 import HomePage from './components/HomePage.vue';
+import App from './Layouts/App.vue'
 
 
 
 // Working now
 const app = createApp({})
 app.component('home-page', HomePage)
+app.component('App', App)
 app.mount('#app')
 
 
